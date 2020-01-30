03145c08b01074026eea4e767573ebea704d2eb6
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-01-30 05:45:43 +0200

    solve the assignment

---
+++ redux-assignment-2/src/containers/Persons.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+class Persons extends Component {
+    state = {
+        persons: []
+    }
+
+    personDeletedHandler = (personId) => {
+        this.setState( ( prevState ) => {
+            return { persons: prevState.persons.filter(person => person.id !== personId)}
+        } );
+    }
+
+    render () {
+        console.log(this.props)
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onAddPerson} />
+                {this.props.persons.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.personDeletedHandler(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state)=>{
+    return {
+        persons: state.persons
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        onAddPerson: ()=> dispatch({type: "ADD_PERSON"})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
---
+++ redux-assignment-2/src/store/reducers/persons.js
@@ -0,0 +1,18 @@
+const initialState = {
+    persons: []
+}
+
+const persons = (state= initialState, action)=>{
+    if(action.type === 'ADD_PERSON'){
+        const newPerson = {
+            id: Math.random(), // not really unique but good enough here!
+            name: 'Max',
+            age: Math.floor( Math.random() * 40 )
+        }
+        
+        return { persons: state.persons.concat(newPerson)};
+    }
+    return state;
+}
+
+export default persons;
