3905f114056f3e1db83f58b8dfafea24b2459574
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-01-30 03:58:34 +0200

    add actions and dispatch them

--- practicing_redux/src/containers/Counter/Counter.js
+++ practicing_redux/src/containers/Counter/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
@@ -8,50 +8,62 @@ class Counter extends Component {
         counter: 0
     }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
+    counterChangedHandler = (action, value) => {
+        switch (action) {
             case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
                 break;
             case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
                 break;
             case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+                this.setState((prevState) => { return { counter: prevState.counter + value } })
                 break;
             case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+                this.setState((prevState) => { return { counter: prevState.counter - value } })
                 break;
             default:
         }
     }
 
-    render () {
+    render() {
         console.log(this.props)
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAddToCounter}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractFromCounter}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 10" clicked={() => this.props.onAddToCounter(10)} />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractFromCounter(5)} />
+                <hr />
+                <ul>
+                    <button onClick={this.props.onStoreResult}>save</button>
+                    {
+                        this.props.results.map((result) => {
+                            return <li onClick ={()=> this.props.onDeleteResult(result.id)} key={result.id} id={result.id}>{result.value}</li>
+                        })
+                    }
+                </ul>
             </div>
         );
     }
 }
 
-const mapStateToProps = (state)=>{
+const mapStateToProps = (state) => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        results: state.results,
     }
 }
 
-const mapDispatchToProps = (dispatch) =>{
-    return{
-        onIncrementCounter: ()=>dispatch({type: 'INC_COUNTER'}),
-        onDecrementCounter: ()=>dispatch({type: 'DEC_COUNTER'}),
-        onAddToCounter: ()=>dispatch({type: 'ADD_TO_COUNTER'}),
-        onSubtractFromCounter: ()=>dispatch({type: 'SUBTRACT_FROM_COUNTER'}),
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onIncrementCounter: () => dispatch({ type: 'INC_COUNTER' }),
+        onDecrementCounter: () => dispatch({ type: 'DEC_COUNTER' }),
+        onAddToCounter: (value) => dispatch({ type: 'ADD_TO_COUNTER', value: value }),
+        onSubtractFromCounter: (value) => dispatch({ type: 'SUBTRACT_FROM_COUNTER', value: value }),
+        onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+        onDeleteResult: (resultId) => dispatch({ type: 'DELETE_RESULT', resultId: resultId}),
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- practicing_redux/src/store/reducer.js
+++ practicing_redux/src/store/reducer.js
@@ -1,31 +1,52 @@
-const intialState ={
-    counter:0
+const intialState = {
+    counter: 0,
+    results: [],
 }
 
-const reducer = (state= intialState, action)=>{
-    console.log('reducer',action);
-    if(action.type === 'INC_COUNTER'){
-        return {
-            counter: state.counter+1
-        }
-    }
-    if(action.type === 'DEC_COUNTER'){
-        return {
-            counter: state.counter-1
-        }
-    }
-    if(action.type === 'ADD_TO_COUNTER'){
-        return {
-            counter: state.counter+5
-        }
-    }
-    if(action.type === 'SUBTRACT_FROM_COUNTER'){
-        return {
-            counter: state.counter-5
-        }
-    }
+const reducer = (state = intialState, action) => {
+    console.log('reducer', action);
 
-    return state;
+    switch (action.type) {
+        case ('INC_COUNTER'):
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+
+        case ('DEC_COUNTER'):
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+
+        case ('ADD_TO_COUNTER'):
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+
+        case ('SUBTRACT_FROM_COUNTER'):
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+        case ('STORE_RESULT'):
+            return {
+                ...state,
+                results: state.results.concat(
+                    { id: (new Date()).getTime().toString(), value: state.counter}
+                )
+            }
+            case ('DELETE_RESULT'):
+                return {
+                    ...state,
+                    results: state.results.filter(
+                        (result)=> result.id !== action.resultId
+                    )
+                }    
+        default:
+            return state;
+    }
 }
 
 
